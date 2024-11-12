d1dcaa034721f8a8a417ff425b47af4f9de3ce4c
gkazimirov396 <grishakazim@gmail.com>
2024-11-12 13:25:36 +0200

    refactor: Extracted pathnames to a separate enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import NoteLayout from './components/NoteLayout';
 import EditNote from './pages/EditNote';
 import NewNote from './pages/NewNote';
 import NoteDetailed from './pages/NoteDetailed';
 import NoteList from './pages/NoteList';
 
+import NoteLayout from './components/NoteLayout';
+
+import { RoutePath } from './router/path';
+
 const App = () => {
   return (
     <main className="container my-10">
       <Routes>
-        <Route path="/" element={<NoteList />} />
-        <Route path="/new-note" element={<NewNote />} />
+        <Route path={RoutePath.HOME} element={<NoteList />} />
+        <Route path={RoutePath.NEW_NOTE} element={<NewNote />} />
 
-        <Route path="/:noteId" element={<NoteLayout />}>
+        <Route path={RoutePath.NOTE_DETAILED} element={<NoteLayout />}>
           <Route index element={<NoteDetailed />} />
-          <Route path="edit" element={<EditNote />} />
+          <Route path={RoutePath.NOTE_DETAILED_EDIT} element={<EditNote />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/" />} />
---
+++ src/router/path.ts
@@ -0,0 +1,6 @@
+export enum RoutePath {
+  HOME = '/',
+  NEW_NOTE = '/new-note',
+  NOTE_DETAILED = '/:noteId',
+  NOTE_DETAILED_EDIT = 'edit',
+}
